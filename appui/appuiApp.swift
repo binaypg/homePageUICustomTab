041d9fbd2c7a5db6d3b6401343bb15d4c6e9abd8
binay <binay.pg@gmail.com>
2021-12-31 15:21:33 +0530

    Initial Commit

---
+++ appui/appuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  appuiApp.swift
+//  appui
+//
+//  Created by Binay Kumar Sharma on 31/12/21.
+//
+
+import SwiftUI
+
+@main
+struct appuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
